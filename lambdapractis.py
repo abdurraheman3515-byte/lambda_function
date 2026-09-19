ddba18bf77a46d9df4ba9b44183197eec6443ab9
Abdurraheman <abdurraheman3515-byte@gmail.com>
2026-09-19 21:54:27 +0530

    lambda_function

---
+++ lambdapractis.py
@@ -0,0 +1,10 @@
+#print((lambda a: a*a)(a=5))
+
+#print((lambda a,b,c:a if a>b and a>c else(b if b>a and b>c else c))(45,55,65))
+
+#print((lambda U:U .upper())("python"))
+
+#print((lambda l:l .lower())("PYTHON"))
+
+#print(list(map((lambda n:0 if n%2==0 else 1),[11,25,66,85,40])))
+
